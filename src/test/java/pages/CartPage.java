e717f20a3dd42e616d0780a5e23febbb7603d511
Sanjaykumar07r <your-email@gmail.com>
2026-05-11 14:10:53 +0530

    verify user credits,verified lockedout user,verified empty credits

--- src/test/java/pages/CartPage.java
+++ src/test/java/pages/CartPage.java
@@ -1,4 +1,54 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public class CartPage {
+    private static WebDriver driver;
+    private final WebDriverWait wait;
+
+    public CartPage(WebDriver driver,WebDriverWait wait){
+        CartPage.driver = driver;
+        this.wait = wait;
+    }
+
+    private final By usernameField = By.id("user-name");
+    private final By passwordField = By.id("password");
+    private final By loginButton = By.id("login-button");
+    private final By errorMessage = By.xpath("//h3[@data-test='error']");
+    private final By menuButton = By.id("react-burger-menu-btn");
+    private final By logoutButton = By.id("logout_sidebar_link");
+
+    public void enterUsername(String username){
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password){
+        driver.findElement(passwordField).sendKeys(password);
+
+    }
+    public void clickLogin(){
+        driver.findElement(loginButton).click();
+    }
+    public void login(String username, String password){
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+    }
+    public String getErrorMessage(){
+        return driver.findElement(errorMessage).getText();
+    }
+
+    public void clickMenu(){
+        driver.findElement(menuButton).click();
+    }
+    public void clickLogout(){
+        driver.findElement(logoutButton).click();
+    }
+    public String getCurrentUrl(){
+        return driver.getCurrentUrl();
+    }
+
+
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,4 +1,32 @@
 package tests;
 
-public class LoginTest {
+import base.BaseTest;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CartPage;
+
+public class LoginTest extends BaseTest {
+
+    @Test
+    public void verifyLogin(){
+        CartPage cartPage = new CartPage(driver, wait);
+        cartPage.enterUsername("standard_user");
+        cartPage.enterPassword("secret_sauce");
+        cartPage.clickLogin();
+    }
+    @Test
+    public void verifyLockedOutUser(){
+        CartPage loginPage = new CartPage(driver, wait);
+        loginPage.login("locked_out_user", "secret_sauce");
+        String error = loginPage.getErrorMessage();
+        Assert.assertTrue(error.contains("locked out"));
+    }
+    @Test
+    public void verifyEmptyCredentials(){
+
+        CartPage loginPage = new CartPage(driver, wait);
+        loginPage.clickLogin();
+        String error = loginPage.getErrorMessage();
+        Assert.assertTrue(error.contains("Username is required"));
+    }
 }
