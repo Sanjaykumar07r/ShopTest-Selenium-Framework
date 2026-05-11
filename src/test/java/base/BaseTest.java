2833a72b2ac5fa12df1d7c7da3bf34189f4a6178
Sanjaykumar07r <your-email@gmail.com>
2026-05-11 21:00:48 +0530

    module 4 and module 5 completed

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -3,6 +3,7 @@ package base;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -15,13 +16,24 @@ public class BaseTest {
     protected WebDriverWait wait;
     protected Actions actions;
 
+
+
     @BeforeMethod
     public void setup() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--disable-save-password-bubble");
+
+        options.addArguments("--disable-notifications");
+
+        options.addArguments("--disable-popup-blocking");
+
+        driver = new ChromeDriver(options);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://www.saucedemo.com/");
+
     }
 
     @AfterMethod
--- src/test/java/pages/CheckoutPage.java
+++ src/test/java/pages/CheckoutPage.java
@@ -1,4 +1,53 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
 public class CheckoutPage {
+
+    WebDriver driver;
+
+    public CheckoutPage(WebDriver driver){
+        this.driver = driver;
+    }
+
+    // Locators
+
+    By checkoutButton = By.id("checkout");
+    By firstName = By.id("first-name");
+    By lastName = By.id("last-name");
+    By postalCode = By.id("postal-code");
+    By continueButton = By.id("continue");
+    By finishButton = By.id("finish");
+    By completeMessage = By.className("complete-header");
+    By summaryProduct = By.className("inventory_item_name");
+    By totalPrice = By.className("summary_total_label");
+
+    public void clickCheckout(){
+        driver.findElement(checkoutButton).click();
+    }
+    public void enterFirstName(String fname){
+        driver.findElement(firstName).sendKeys(fname);
+    }
+    public void enterLastName(String lname){
+        driver.findElement(lastName).sendKeys(lname);
+    }
+    public void enterPostalCode(String code){
+        driver.findElement(postalCode).sendKeys(code);
+    }
+    public void clickContinue(){
+        driver.findElement(continueButton).click();
+    }
+    public String getSummaryProductName(){
+        return driver.findElement(summaryProduct).getText();
+    }
+    public String getTotalPrice(){
+        return driver.findElement(totalPrice).getText();
+    }
+    public void clickFinish(){
+        driver.findElement(finishButton).click();
+    }
+    public String getConfirmationMessage(){
+        return driver.findElement(completeMessage).getText();
+    }
 }
