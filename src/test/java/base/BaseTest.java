53c48ff5a8f880bac619044b1114434e5942ef70
Sanjaykumar07r <your-email@gmail.com>
2026-05-12 15:01:09 +0530

    test reports1

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,6 +1,7 @@
 package base;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
@@ -39,7 +40,7 @@ public class BaseTest {
             wait = new WebDriverWait(driver, Duration.ofSeconds(10));
             actions = new Actions(driver);
             config = new ConfigReader();
-            driver.manage().window().maximize();
+            driver.manage().window().setSize(new Dimension(1920,1080));
             driver.manage().deleteAllCookies();
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 //            driver.get("https://www.saucedemo.com/");
